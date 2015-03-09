5d949520f5c15e26d3e63a4c56f231deb197a433
Jens <jens.vingen@gmail.com>
2015-03-09 12:45:20 +0100

    Endra massvis igjen

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -6,6 +6,11 @@
 
 public class SecondClass {
 
+    private int face;
+
+    private int coffee;
+    private int soda;
+
     public SecondClass(int face, String coffee) {
         this.face = face;
         this.coffee = coffee;
