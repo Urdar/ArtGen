e1c52d563d65e1e9141a010aa3bc1e42b8ab6db2
Jens <jens.vingen@gmail.com>
2015-03-09 12:22:49 +0100

    Endra massvis

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -3,8 +3,15 @@ package src;
 /**
  * Created by Gøran on 09.03.2015.
  * Jens: Jarle er GAY
+ * Jens: Ivan er GAY
  */
 
 public class SecondClass {
     // (O.O)
+    // Jensemannen start
+    // ..
+    // ..
+    // ..
+    // ..
+    // Jensemannen end
 }
