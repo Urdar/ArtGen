5708075d401996782fabbf67f5928bb9f3bd4e27
Jens <jens.vingen@gmail.com>
2015-03-09 12:46:18 +0100

    Endra massvis igjen

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,5 +1,6 @@
 /**
  * Created by Gøran on 09.03.2015.
+ * Jens: Ivan er GAY
  * hahahaha *hulk*
  */
 
@@ -10,7 +11,7 @@ public class SecondClass {
     private int coffee;
     private int soda;
 
-    public SecondClass(int face, String coffee) {
+    public SecondClass(int face, int coffee) {
         this.face = face;
         this.coffee = coffee;
         this.soda = soda;
